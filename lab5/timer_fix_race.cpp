3683341dff4cc949b42a976c1957a66f3383fb96
WonGu <namwg7@hanyang.ac.kr>
2016-10-14 14:22:44 +0900

    lab 5

--- lab5/timer_fix_race.cpp
+++ lab5/timer_fix_race.cpp
@@ -27,7 +27,7 @@ public:
 
             timer1_.expires_at(timer1_.expires_at()
                     + boost::posix_time::seconds(1));
-            timer1_.async_wait(boost::bind(&Printer::Print1, this));
+            timer1_.async_wait(strand_.wrap(boost::bind(&Printer::Print1, this)));
         }
     }
 
@@ -38,7 +38,7 @@ public:
 
             timer2_.expires_at(timer2_.expires_at()
                     + boost::posix_time::seconds(1));
-            timer2_.async_wait(boost::bind(&Printer::Print2, this));
+            timer2_.async_wait(strand_.wrap(boost::bind(&Printer::Print2, this)));
         }
     }
 
